d0f13dd7098f8389287809dcff420cccd81ced1f
Yunhans <91072133+Yunhans@users.noreply.github.com>
2021-10-09 19:00:31 +0800

    c

---
+++ c/110403043_C1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>                  /* stdio.h 標頭檔 */
+#include <stdlib.h>                 /* stdlib.h 標頭檔 */
+int main(void){                     /* main 主函式 */
+    printf("Hello C!\n");           
+    printf("Hello World!\n");
+
+    system("pause");
+    return 0;
+}
---
+++ c/110403043_HW3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>                 			/* stdio.h 標頭檔 */
+#include <stdlib.h>                 		/* stdlib.h 標頭檔 */
+int main(void){                    			/* main 主函式 */
+    int i = 5;								/* 宣告整數 */
+    int j = 12;								/* 宣告整數 */
+    printf("%d + %d = %d\n",i,j,i+j); 		/* i + j = i+j */
+    system("pause");						/* 暫停讓我看看視窗 */
+    return 0;								/* 結束 */
+}
