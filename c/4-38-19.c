7ba0fd8c714346fee2de71db256a85f935475e48
Yunhans <91072133+Yunhans@users.noreply.github.com>
2021-10-29 14:02:33 +0800

    push

---
+++ c/4-38-19.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    char ch;
+
+    printf("Input a charcter");
+    ch = getchar();
+    printf("ch=%c,ASCII code is %d\n",ch,ch);
+    
+    system("pause");
+    return 0;
+}
